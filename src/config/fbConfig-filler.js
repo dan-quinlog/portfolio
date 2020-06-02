f64d246e757dbc74067d55fce6915780d4bd77b6
Tombo Yuuko <dan.quinlog@gmail.com>
2020-06-02 17:05:44 -0600

    updated with filler api data

---
+++ src/config/fbConfig-filler.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
